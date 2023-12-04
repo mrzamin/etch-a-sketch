dacba6a7600380cddf95919021766f0dae5a13d0
mrzamin <142754418+mrzamin@users.noreply.github.com>
2023-12-04 18:23:50 -0500

    Modify genDivs function

--- main.js
+++ main.js
@@ -2,11 +2,19 @@
 
 const container = document.querySelector('.container');
 
+
 function genDivs (number) {
-    for(let i = 0; i < (number * number); i++) {
-        const gridItem = document.createElement('div');
-        gridItem.classList.add('gridItem');
-        container.appendChild(gridItem);
+    for(let i = 0; i < number; i++) {
+        let gridRow = document.createElement('div');
+        gridRow.classList.add('row');
+        container.appendChild(gridRow);
+        for (let k = 0; k < number; k++) {
+            let gridColumn = document.createElement('div');
+            gridColumn.classList.add('column');
+            gridColumn.addEventListener ('mouseover', () =>
+            {gridColumn.style.backgroundColor = "pink"});
+            gridRow.appendChild(gridColumn);
+        }
     }
 }
 
